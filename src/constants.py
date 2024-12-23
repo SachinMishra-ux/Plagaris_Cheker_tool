c80dc1b09c3749b0265ac49034a62cc045fbbcd4
Sachin <sachin.mishra@datasociety.com>
2024-12-23 17:52:04 +0530

    full path added

--- src/constants.py
+++ src/constants.py
@@ -1,3 +1,3 @@
 chrome_driver_path = "./local_driver/chromedriver" #chrome driver path
 # Define the directory for storing images
-image_folder = "/Users/sachinmishra/Desktop/Testing/plagarism_checker/extracted_images"
+image_folder = "/Users/sachinmishra/Desktop/plagarism_checker/extracted_images"
